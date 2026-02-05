d001fa93e0dbcce79a907ec39d437f87132c0834
Carlos <perdomomoralescarlos@gmail.com>
2026-02-05 15:47:21 +0000

    Añadido los posibles estados de pedidos

---
+++ Preset/EstadoPedido.java
@@ -0,0 +1,9 @@
+package Preset;
+//Predefinidos para el Estado de pedido
+public enum EstadoPedido{
+PEDIDO_EN_PREPARACION,
+PEDIDO_EN_ESPERA,
+PEDIDO_EN_ESPERA_DE_PAGO,
+PEDIDO_ENVIADO,
+PEDIDO_ENTREGADO
+}
