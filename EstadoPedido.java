2b6dd2416b7d3824d0e4802a134e2e46ae3f1ff1
Damian Perez <Damian123@gmail.com>
2026-02-10 22:26:34 +0000

    Este es el archivo MAIN

---
+++ EstadoPedido.java
@@ -0,0 +1,8 @@
+package Preset;
+//Predefinidos para el Estado de pedido
+public enum EstadoPedido{
+PEDIDO_EN_ESPERA,
+PEDIDO_EN_PREPARACION,
+PEDIDO_ENVIADO,
+PEDIDO_ENTREGADO
+}
---
+++ MAIN.java
@@ -0,0 +1,25 @@
+public static void main(String[] args){
+    System.out.println("=== SISTEMA DE GESTIÓN DE PEDIDOS ===");
+
+    Pedido p1 = new Pedido("Paquito");
+
+        p1.agregarArticulo("Hamburguesa Gourmet", 1, 2.50);
+        p1.agregarArticulo("Patatas Rústicas", 5, 15.95);
+        p1.agregarArticulo("Refresco Cola", 5, 5.50);
+        p1.agregarArticulo("Tarta de Queso", 1, 10.00);
+        p1.agregarArticulo("Café Solo", 1, 1.60);
+        p1.agregarArticulo("Helado", 1, 3.00);
+        p1.mostrarResumen();
+
+        System.out.println("=== PRUEBA DE ESTADOS ===");
+        p1.setEstado(EstadoPedido.LISTO_PARA_ENTREGAR);
+        System.out.println("Estado actualizado a: " + p1.getEstado());
+        p1.setEstado(EstadoPedido.ENTREGADO);
+        System.out.println("Estado actual: " + p1.getEstado());
+        p1.setEstado(EstadoPedido.EN_PREPARACION);
+        System.out.println("\n=== SEGUNDO PEDIDO ===");
+
+        Pedido p2 = new Pedido("Agustina");
+        p2.agregarArticulo("Sushi Combo", 1, 20.00);
+        p2.mostrarResumen();
+}
