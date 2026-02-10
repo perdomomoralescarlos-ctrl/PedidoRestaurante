fb8161104d67f68360859694ffad7dbbcba7ea66
Carlos <perdomomoralescarlos@gmail.com>
2026-02-10 16:36:36 +0000

    Cambiado el archivo EstadoPedido.java

--- Preset/EstadoPedido.java
+++ Preset/EstadoPedido.java
@@ -1,9 +1,8 @@
 package Preset;
 //Predefinidos para el Estado de pedido
 public enum EstadoPedido{
-PEDIDO_EN_PREPARACION,
 PEDIDO_EN_ESPERA,
-PEDIDO_EN_ESPERA_DE_PAGO,
+PEDIDO_EN_PREPARACION,
 PEDIDO_ENVIADO,
 PEDIDO_ENTREGADO
 }
