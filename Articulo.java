85c151e485d9637eb5b8a0eb58bd8e069eff74fe
Carlos <perdomomoralescarlos@gmail.com>
2026-02-12 15:01:27 +0000

    Añadido el archivo articulos.java

---
+++ Articulo.java
@@ -0,0 +1,27 @@
+package Preset;
+
+public class Articulo {
+    private String nombre;
+    private int cantidad;
+    private double precioUnitario;
+
+    public Articulo(String nombre, int cantidad, double precioUnitario){
+        this.nombre= nombre;
+        this.cantidad= cantidad;
+        this.precioUnitario= precioUnitario;
+    }
+
+    public double calcularSubtotal(){
+        return cantidad * precioUnitario;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    public int getCantidad(){
+        return cantidad;
+    }
+    public double getPrecioUnitario(){
+        return precioUnitario;
+    }
+}
